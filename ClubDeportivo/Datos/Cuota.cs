4d1a5eb77b4d547a0c7600eb9c4121795f59eba8
Estiven Fernandez <estivenfs@gmail.com>
2024-11-07 21:13:01 -0300

    Funcion para listar los vencimientos

--- ClubDeportivo/Datos/Cuota.cs
+++ ClubDeportivo/Datos/Cuota.cs
@@ -1,6 +1,8 @@
 ﻿using ClubDeportivo.Entidades;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +22,43 @@ namespace ClubDeportivo.Datos
         {
             return true;
         }
-
+        
         public List<E_Cuota_Cliente> listarVencimientos()
         {
-            return new List<E_Cuota_Cliente>();
+            MySqlConnection sqlCon = new MySqlConnection();
+            try
+            {
+                sqlCon.Open();
+                string query = "SELECT * FROM cuotas WHERE fechaVencimiento = CURDATE()";
+                MySqlCommand cmd = new MySqlCommand(query, sqlCon);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                List<E_Cuota_Cliente> cuotas = new List<E_Cuota_Cliente>();
+                while (reader.Read()) {
+                    E_Cuota_Cliente cuota = new E_Cuota_Cliente();
+                    cuota.cuota = new E_Cuota();
+                    cuota.cuota.idCuota = reader.GetInt32("idCuota");
+                    cuota.cuota.fechaUltimoPago = reader.GetDateTime("fechaUltimoPago");
+                    cuota.cuota.fechaVencimiento = reader.GetDateTime("fechaVencimiento");
+                    cuota.cuota.valorCuota = reader.GetFloat("valorCuota");
+                    cuota.cuota.formaPago = reader.GetString("formaPago");
+                    cuota.cuota.idCliente = reader.GetInt32("idCliente");
+                    cuotas.Add(cuota);
+                }
+                return cuotas;
+                
+            }catch (Exception ex) {
+                throw new Exception("Error al intentar listar las cuotas vencidas: " + ex.Message);
+
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+
+            }
+
         }
     }
 }

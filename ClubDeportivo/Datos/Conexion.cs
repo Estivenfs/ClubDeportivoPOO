503500d149c7a0968dab857634fac29873e76de2
Estiven Fernandez <estivenfs@gmail.com>
2024-11-13 21:11:12 -0300

    Puesta a punto

--- ClubDeportivo/Datos/Conexion.cs
+++ ClubDeportivo/Datos/Conexion.cs
@@ -19,11 +19,41 @@ namespace ClubDeportivo.Datos
 
         private Conexion() // asignamos valores a las variables de la conexion
         {
+            bool correcto = false;
+            int mensaje;
+
+            string T_servidor = "localhost";
+            string T_puerto = "3306";
+            string T_usuario = "root";
+            string T_clave = "";
+
+            while (!correcto) {
+                T_servidor = Microsoft.VisualBasic.Interaction.InputBox("Ingrese el servidor", "DATOS DE INSTALACION MySQL");
+                T_puerto = Microsoft.VisualBasic.Interaction.InputBox("Ingrese el puerto", "DATOS DE INSTALACION MySQL");
+                T_usuario = Microsoft.VisualBasic.Interaction.InputBox("Ingrese el usuario", "DATOS DE INSTALACION MySQL");
+                T_clave = Microsoft.VisualBasic.Interaction.InputBox("Ingrese la clave", "DATOS DE INSTALACION MySQL");
+
+                mensaje = (int)MessageBox.Show("Su ingreso: Servidor: " + T_servidor + "\nPuerto: " + T_puerto + "\nUsuario: " + T_usuario + "\nClave: " + T_clave, "AVISO DEL SISTEMA", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if ( mensaje != 6)
+                {
+                    MessageBox.Show("Por favor, ingrese nuevamente los datos", "AVISO DEL SISTEMA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    correcto = false;
+                }
+                else
+                {
+                    correcto = true;
+                }
+
+            }
+
+
             this.baseDatos = "Proyecto";
-            this.servidor = "localhost";
-            this.puerto = "3306";
-            this.usuario = "root";
-            this.clave = "";
+            this.servidor = T_servidor;
+            this.puerto = T_puerto;
+            this.usuario = T_usuario;
+            this.clave = T_clave;
+            
         }
 
         public MySqlConnection getConexion() // creamos la conexion
